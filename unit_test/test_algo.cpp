98be9ef4eb88abf790db9d5d2d80d00b17a91cb6
Antonin <antonin.raffin@ensta-paristech.fr>
2016-10-15 13:02:45 +0200

    Implement quicksort, no performance improvement

--- unit_test/test_algo.cpp
+++ unit_test/test_algo.cpp
@@ -7,13 +7,9 @@
 #include <assert.h>
 #include "test_algo.h"
 
+#define PI 3.14159265359
+
 using namespace std;
-
-/* Selection function
-So far I have considered that this function will classify the best member of X_k and Z at the fist positions
-ie P will be the mu first columns of X_k and Z (to avoid having new matrix) but I don't know if it is the best way
-
-*/
 
 double normE(double *m, size_t dimension)
 {
@@ -60,7 +56,103 @@ void freeMatrix(double** mat, size_t nb_rows)
 }
 
 
-#define PI 3.14159265359
+// void quicksort(vector<int>& vec, int st, int end) {
+//   if(st == end) return;
+// 
+//   int sep = st;
+//   for(int i = st + 1; i < end; i++) {
+//     if(vec[i] < vec[st]) swap(vec[++sep], vec[i]);
+//   }
+// 
+//   swap(vec[st], vec[sep]);
+//   quicksort(vec, st, sep);
+//   quicksort(vec, sep + 1, end);
+// }
+
+void quickSort(double* fitness, double** X_k, double** Z,\
+              size_t left, size_t right, size_t dimension)
+{
+    size_t i = left, j = right;
+    double tmp;
+    double pivot = fitness[(left + right + 1) / 2];
+    /* Partition */
+    while (i <= j)
+    {
+      while (fitness[i] < pivot)
+        i++;
+
+      while (fitness[j] > pivot)
+        j--;
+
+      if (i <= j)
+      {
+        // Swap fitness
+        tmp = fitness[i];
+        fitness[i] = fitness[j];
+        fitness[j] = tmp;
+        for (size_t l = 0; l < dimension; l++)
+        {
+          // swap X
+          tmp = X_k[i][l];
+          X_k[i][l] = X_k[j][l];
+          X_k[j][l] = tmp;
+          // swap Z
+          tmp = Z[i][l];
+          Z[i][l] = Z[j][l];
+          Z[j][l] = tmp;
+        }
+        i++;
+        j--;
+      }
+    };
+
+    /* Recursion */
+    if (left < j)
+      quickSort(fitness, X_k, Z, left, j, dimension);
+
+    if (i < right)
+      quickSort(fitness, X_k, Z, i, right, dimension);
+}
+
+void quickSort(double* fitness, size_t* indices,\
+              size_t left, size_t right)
+{
+    size_t i = left, j = right;
+    double tmp;
+    size_t idx;
+    double pivot = fitness[(left + right + 1) / 2];
+    /* Partition */
+    while (i <= j)
+    {
+      while (fitness[i] < pivot)
+        i++;
+
+      while (fitness[j] > pivot)
+        j--;
+
+      if (i <= j)
+      {
+        // Swap fitness
+        tmp = fitness[i];
+        fitness[i] = fitness[j];
+        fitness[j] = tmp;
+        // Swap indices
+        idx = indices[i];
+        indices[i] = indices[j];
+        indices[j] = idx;
+        
+        i++;
+        j--;
+      }
+    };
+
+    /* Recursion */
+    if (left < j)
+      quickSort(fitness, indices, left, j);
+
+    if (i < right)
+      quickSort(fitness, indices, i, right);
+}
 
 void algo4(void (*evaluate)(double*, double*),
                     const size_t dimension,
@@ -84,7 +176,7 @@ void algo4(void (*evaluate)(double*, double*),
   double s_sigma[dimension]; // search path --> vector !
   bool happy = false;
   int counter = 0;
-  size_t lambda = 40;
+  size_t lambda = 50;
   size_t mu = (size_t) lambda/4;
   // Damping factors
   double d = 1. + sqrt((double)mu/(double)dimension); 
@@ -94,6 +186,8 @@ void algo4(void (*evaluate)(double*, double*),
   double** X_k;
   // Mutation vectors
   double** Z;
+  double** X_k_tmp;
+  double** Z_k_tmp;
   double fitness[lambda];
   double E_HALF_NORMAL = sqrt(2./PI);
   double _dim = double(dimension);
@@ -101,10 +195,14 @@ void algo4(void (*evaluate)(double*, double*),
   // Matrix Initialization
   X_k = new double*[lambda];
   Z = new double*[lambda];
+  Z_k_tmp = new double*[lambda];
+  X_k_tmp = new double*[lambda];
   for (size_t k = 0; k < lambda; k++)
   {
     X_k[k] = new double[dimension];
     Z[k] = new double[dimension];
+    Z_k_tmp[k] = new double[dimension];
+    X_k_tmp[k] = new double[dimension];
   }
 
   // Random generator (normal distribution)
@@ -139,74 +237,109 @@ void algo4(void (*evaluate)(double*, double*),
         X_k[k][j] = fmax(lower_bounds[j], fmin(X_k[k][j], upper_bounds[j]));
       }
     }
+    
+    size_t indices[lambda];
     // Evaluate the solutions
     for (size_t k = 0; k < lambda; k++)
     {
       evaluate(X_k[k], y);
       fitness[k] = y[0];
+      indices[k] = k;
+      for (size_t j = 0; j < dimension; j++)
+      {
+        X_k_tmp[k][j] = X_k[k][j];
+        Z_k_tmp[k][j] = Z[k][j];
+      }
+    }
+    
+    // quickSort(fitness, X_k, Z, 0, lambda-1, dimension);
+    quickSort(fitness, indices, 0, lambda-1);
+    
+    for (size_t k = 0; k < mu; k++)
+    {
+      size_t idx = indices[k];
+      for (size_t j = 0; j < dimension; j++)
+      {
+        X_k[k][j] = X_k_tmp[idx][j];
+        Z[k][j] = Z_k_tmp[idx][j];
+      }
     }
     
     // Select the mu best (shell sort)
     // Shell sort : better when using greater lambda
-    int gaps[8] = {701, 301, 132, 57, 23, 10, 4, 1};
-
-    // Start with the largest gap and work down to a gap of 1
-    for (size_t g = 0; g < 8; g++)
-    {
-      size_t gap = gaps[g];
-      // Do a gapped insertion sort for this gap size.
-      // The first gap elements fitness[0..gap-1] are already in gapped order
-      // keep adding one more element until the entire array is gap sorted
-      for (size_t k = gap; k < lambda; k += 1)
-      {
-        double y_tmp = fitness[k];
-        // X_tmp = X_k[k]
-        vectorCopy(X_k[k], X_tmp, dimension);
-        // Z_tmp = Z[k]
-        vectorCopy(Z[k], Z_tmp, dimension);
-        size_t pos = k;
-        // shift earlier gap-sorted elements up 
-        // until the correct location for fitness[k] is found
-        while(pos >= gap && fitness[pos-gap] > y_tmp)
-        {
-          fitness[pos] = fitness[pos-gap];
-          vectorCopy(X_k[pos-gap], X_k[pos], dimension);
-          vectorCopy(Z[pos-gap], Z[pos], dimension);
-          pos -= gap;
-        }
-        // Repostion f(x_k)
-        fitness[pos] = y_tmp;
-        // Reposition x_k : X_k[pos] = X_tmp
-        vectorCopy(X_tmp, X_k[pos], dimension);
-        // Reposition z_k : Z[pos] = Z_tmp
-        vectorCopy(Z_tmp, Z[pos], dimension);
-      }
-    }
+    // int gaps[8] = {701, 301, 132, 57, 23, 10, 4, 1};
+    // 
+    // // Start with the largest gap and work down to a gap of 1
+    // for (size_t g = 0; g < 8; g++)
+    // {
+    //   size_t gap = gaps[g];
+    //   // Do a gapped insertion sort for this gap size.
+    //   // The first gap elements fitness[0..gap-1] are already in gapped order
+    //   // keep adding one more element until the entire array is gap sorted
+    //   for (size_t k = gap; k < lambda; k += 1)
+    //   {
+    //     double y_tmp = fitness[k];
+    //     for (size_t j = 0; j < dimension; j++)
+    //     {
+    //       X_tmp[j] = X_k[k][j];
+    //       Z_tmp[j] = Z[k][j];
+    //     }
+    //     size_t pos = k;
+    //     // shift earlier gap-sorted elements up 
+    //     // until the correct location for fitness[k] is found
+    //     while(pos >= gap && fitness[pos-gap] > y_tmp)
+    //     {
+    //       fitness[pos] = fitness[pos-gap];
+    //       for (size_t j = 0; j < dimension; j++)
+    //       {
+    //         X_k[pos][j] = X_k[pos-gap][j];
+    //         Z[pos][j] = Z[pos-gap][j];
+    //       }
+    //       pos -= gap;
+    //     }
+    //     // Reposition f(x_k)
+    //     fitness[pos] = y_tmp;
+    //     for (size_t j = 0; j < dimension; j++)
+    //     {
+    //       // Reposition x_k : X_k[pos] = X_tmp
+    //       X_k[pos][j] = X_tmp[j];
+    //       // Reposition z_k : Z[pos] = Z_tmp
+    //       Z[pos][j] = Z_tmp[j];
+    //     }
+    //   }
+    // }
 
     // Select the mu best (insertion sort)
     // Better when lambda small
     // for (size_t k = 0; k < lambda-1; k++)
     // {
     //   double y_tmp = fitness[k];
-    //   // X_tmp = X_k[k]
-    //   vectorCopy(X_k[k], X_tmp, dimension);
-    //   // Z_tmp = Z[k]
-    //   vectorCopy(Z[k], Z_tmp, dimension);
+    //   for (size_t j = 0; j < dimension; j++)
+    //   {
+    //     X_tmp[j] = X_k[k][j];
+    //     Z_tmp[j] = Z[k][j];
+    //   }
     //   
     //   size_t pos = k;
     //   while (pos > 0 && fitness[pos-1] > y_tmp)
     //   {
     //     fitness[pos] = fitness[pos-1];
-    //     vectorCopy(X_k[pos-1], X_k[pos], dimension);
-    //     vectorCopy(Z[pos-1], Z[pos], dimension);
+    //     for (size_t j = 0; j < dimension; j++)
+    //     {
+    //       X_k[pos][j] = X_k[pos-1][j];
+    //       Z[pos][j] = Z[pos-1][j];
+    //     }
     //     pos--;
     //   }
-    //   // Repostion f(x_k)
+    //   // Reposition f(x_k)
     //   fitness[pos] = y_tmp;
-    //   // Reposition x_k : X_k[pos] = X_tmp
-    //   vectorCopy(X_tmp, X_k[pos], dimension);
-    //   // Reposition z_k : Z[pos] = Z_tmp
-    //   vectorCopy(Z_tmp, Z[pos], dimension);
+    //   for (size_t j = 0; j < dimension; j++)
+    //   {
+    //     // Reposition x_k : X_k[pos] = X_tmp
+    //     X_k[pos][j] = X_tmp[j];
+    //     // Reposition z_k : Z[pos] = Z_tmp
+    //     Z[pos][j] = Z_tmp[j];
+    //   }
     // }
 
     // update s_sigma
@@ -247,11 +380,15 @@ void algo4(void (*evaluate)(double*, double*),
     counter++;
   }
   evaluate(X,y);
+
+  // printArray(fitness,dimension);
   printArray(X,dimension);
   printArray(y,number_of_objectives);
   // Free memory
   freeMatrix(X_k, lambda);
   freeMatrix(Z, lambda);
+  freeMatrix(X_k_tmp, lambda);
+  freeMatrix(Z_k_tmp, lambda);
 }
 
 void poly(double* x, double* y)
@@ -284,12 +421,12 @@ void euclidianNorm(double* x, double* y)
 int main(int argc, char const *argv[]) {
   size_t dim = 40;
   size_t number_of_objectives = 1;
-  size_t budget = 1000;
+  size_t budget = 20000;
   double lower[dim];
   double upper[dim];
   for (size_t j = 0; j < dim; j++) {
-    lower[j] = -10;
-    upper[j] = 10;
+    lower[j] = -1000;
+    upper[j] = 1000;
   }
   algo4(euclidianNorm, dim, number_of_objectives, lower, upper, budget);
   return 0;
